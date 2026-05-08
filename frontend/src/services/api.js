084aacdc952458b47f2e2cafecc91d64dd5d0000
rajpal-singh-code <rajpalku2022@gmail.com>
2026-05-08 23:48:06 +0530

    add backend url

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,11 +1,18 @@
 import axios from 'axios'
 
+
+const BACKEND_VERCEL_URL = 'https://your-backend-project-name.vercel.app/api';
+const LOCAL_URL = 'http://localhost:5000/api';
+
+
+const API_URL = window.location.hostname === 'localhost' ? LOCAL_URL : BACKEND_VERCEL_URL;
+
 const API = axios.create({
-  baseURL: 'http://localhost:5000/api',
+  baseURL: API_URL,
   withCredentials: true,
 })
 
-// Add this interceptor to attach the token to every request
+
 API.interceptors.request.use((config) => {
   const token = localStorage.getItem('token');
   if (token) {
