30abe838f91506d65f0ee43463e323204c6ecee1
rajpal-singh-code <rajpalku2022@gmail.com>
2026-06-11 10:21:46 +0530

    backend url in frontend

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,25 +1,29 @@
-import axios from 'axios'
+import axios from 'axios';
 
-
-const BACKEND_VERCEL_URL = 'https://ai-pdf-chat-theta.vercel.app/api'; 
+const BACKEND_VERCEL_URL = 'https://ai-pdf-chat-umber.vercel.app/api';
 const LOCAL_URL = 'http://localhost:5000/api';
 
-const API_URL = window.location.hostname === 'localhost' ? LOCAL_URL : BACKEND_VERCEL_URL;
+const API_URL =
+  window.location.hostname === 'localhost'
+    ? LOCAL_URL
+    : BACKEND_VERCEL_URL;
 
 const API = axios.create({
   baseURL: API_URL,
   withCredentials: true,
-})
-
-
-API.interceptors.request.use((config) => {
-  const token = localStorage.getItem('token');
-  if (token) {
-    config.headers.Authorization = `Bearer ${token}`;
-  }
-  return config;
-}, (error) => {
-  return Promise.reject(error);
 });
+
+API.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem('token');
+
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+
+    return config;
+  },
+  (error) => Promise.reject(error)
+);
 
 export default API;
