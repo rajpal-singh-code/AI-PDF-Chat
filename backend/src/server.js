f027ebe2561f6f37d865a08bab0c0cb81c662a29
rajpal-singh-code <rajpalku2022@gmail.com>
2026-06-11 10:28:24 +0530

    Update CORS origins

--- backend/src/server.js
+++ backend/src/server.js
@@ -9,11 +9,10 @@ const app = express();
 const authRouter = require("./routes/authRoutes.js");
 const pdfRoutes = require("./routes/pdfRoutes.js");
 
-// ✅ Cleaned CORS Configuration
 app.use(cors({
     origin: [
-        "http://localhost:5173", 
-        "https://ai-pdf-chat-zk38.vercel.app" 
+        "http://localhost:5173",
+        "https://ai-pdf-chat-jug8.vercel.app"
     ],
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE"],
@@ -30,13 +29,16 @@ app.get("/", (req, res) => {
 app.use("/api", authRouter);
 app.use("/api", pdfRoutes);
 
-const startServer = async() => {
+const startServer = async () => {
     try {
         await connectDB();
+
         const port = process.env.PORT || 5000;
+
         app.listen(port, () => {
             console.log(`Server is running on port ${port}`);
-        })
+        });
+
     } catch (error) {
         console.error("Error starting server:", error.message);
         process.exit(1);
